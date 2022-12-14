01a237d0896cdfdeac950174f4384f30a8e76896
X Not For U <xnotforyou08@gmail.com>
2022-12-14 19:58:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 
-pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
+apt install imagemagick git nodejs ffmpeg libwebp mc nano
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt install --no-install-recommends yarn
 rm -rf node_modules 
 yarn
 rm -rf session.json 
